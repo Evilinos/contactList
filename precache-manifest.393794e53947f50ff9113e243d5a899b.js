f9eb380852f61fbe0c3211774d1e19f34d33ea06
Evilinos <e-streltsov@mail.ru>
2020-05-28 20:19:14 +0300

    Updates

---
+++ precache-manifest.393794e53947f50ff9113e243d5a899b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60b9fa022a98405eff11a3cfd14603e",
+    "url": "/Evilinos/tz/index.html"
+  },
+  {
+    "revision": "4781d721e6839248b138",
+    "url": "/Evilinos/tz/static/css/main.2995726c.chunk.css"
+  },
+  {
+    "revision": "711d149311630d888026",
+    "url": "/Evilinos/tz/static/js/2.18437302.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Evilinos/tz/static/js/2.18437302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4781d721e6839248b138",
+    "url": "/Evilinos/tz/static/js/main.1836d05e.chunk.js"
+  },
+  {
+    "revision": "013d5fb25e0c6326fb8e",
+    "url": "/Evilinos/tz/static/js/runtime-main.1a8b0c76.js"
+  },
+  {
+    "revision": "de470447f4fe98efd7e02bccb5b42bae",
+    "url": "/Evilinos/tz/static/media/avatar.de470447.png"
+  }
+]);
