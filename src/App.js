c166433b48b8761bfc37d0a50d9a172d647147b4
Evilinos <e-streltsov@mail.ru>
2020-06-02 14:18:59 +0300

    add gh-pages and readme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './App.css';
 import Login from "./components/login/Login";
 import {Redirect, Route, Switch} from 'react-router-dom';
@@ -6,16 +6,15 @@ import ContactsContainer from "./components/contacts/ContactsContainer";
 
 const App = () => {
   return (
-      <Suspense fallback={<div>Загрузка</div>}>
         <Switch>
             <Route exact path='/login'
             render={() =>  <Login />}/>
             <Route exact path='/contacts'
                    render={() =>  <ContactsContainer/>}/>
             <Redirect exact from="/" to="/login" />
+            <Redirect exact from="/tz/" to="/login" />
             <Route render={() => <div>404 NOT FOUND</div>}/>
         </Switch>
-      </Suspense>
   );
 };
 
